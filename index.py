46c4394b7c83468d47c559a1331b8be5a5b907b7
Apoorv Singh <67574086+Apoorv-singh-coding@users.noreply.github.com>
2026-06-18 01:36:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
@@ -28,10 +29,20 @@ embedding_model=OpenAIEmbeddings(
 )
 
 
-vector_store=QdrantVectorStore.from_documents(
+"""vector_store=QdrantVectorStore.from_documents(
     documents=chunks,
     embedding=embedding_model,
     url="http://localhost:6333",
+    collection_name="learning_RAG"
+)"""
+
+
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url=os.getenv("https://659ff597-a9b3-4c49-a522-5d054b5dc118.us-west-1-0.aws.cloud.qdrant.io"),
+    api_key=os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6NjEwZTQzODAtMjE2MS00MjlkLWE3NjktZTM1MDNkODVmZjIwIn0._ocfkW9VvGhuQA8To1TzA7gWtgxHxhpL1JJ52UOVh3g"),
     collection_name="learning_RAG"
 )
 
