a6ae9d997a4af184ead3369f0084e30455b47e1d
Apoorv Singh <67574086+Apoorv-singh-coding@users.noreply.github.com>
2026-06-18 19:03:52 +0530

    Update index.py

--- index.py
+++ index.py
@@ -41,8 +41,8 @@ embedding_model=OpenAIEmbeddings(
 vector_store = QdrantVectorStore.from_documents(
     documents=chunks,
     embedding=embedding_model,
-    url=os.getenv("https://659ff597-a9b3-4c49-a522-5d054b5dc118.us-west-1-0.aws.cloud.qdrant.io"),
-    api_key=os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6NjEwZTQzODAtMjE2MS00MjlkLWE3NjktZTM1MDNkODVmZjIwIn0._ocfkW9VvGhuQA8To1TzA7gWtgxHxhpL1JJ52UOVh3g"),
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
     collection_name="learning_RAG"
 )
 
